19956b597cc821df066ff983a4906981e002db36
armin.bayat <arminbayat@yahoo.com>
2020-01-31 22:30:59 +0330

    improve test by adding and expect method

--- src/test/java/com/bayat/armin/springwebservice/controller/HelloControllerTest.java
+++ src/test/java/com/bayat/armin/springwebservice/controller/HelloControllerTest.java
@@ -7,11 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloController.class)
@@ -27,8 +27,11 @@ public class HelloControllerTest {
                 .get("/hello-world")
                 .accept(MediaType.APPLICATION_JSON);
 
-        MvcResult result = mockMvc.perform(request).andReturn();
+        mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andReturn();
 
-        assertEquals("Hello World", result.getResponse().getContentAsString());
     }
 }
